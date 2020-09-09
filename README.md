fee7f5e5f17ca1dafddbe3210219e42f5d716cdc
Sergio Medina <aquitania99@gmail.com>
2020-09-10 01:06:40 +1000

    Iconic Backend technical challenge - WIP

---
+++ README.md
@@ -0,0 +1,93 @@
+# Purpose
+Create a web environment, to work with Symfony + MySQL + Nginx
+
+# Requirements
+* Docker
+* MySQL Workbench (or any other Database client)
+
+# Project Definition
+The project skeleton has been created with symfony command to start a project from scratch:
+```bash
+your-project/
+├── assets/
+├── bin/
+│ └── console
+├── config/
+│ ├── bundles.php
+│ ├── packages/
+│ ├── routes.yaml
+│ └── services.yaml
+├── public/
+│ └── index.php
+├── src/
+│ ├── ...
+│ └── Kernel.php
+├── templates/
+├── tests/
+├── translations/
+├── var/
+│ ├── cache/
+│ └── tmp/
+└── vendor/
+```
+**`Important:`** The **/docker** folder has the PHP & Nginx services configuration used by docker-compose.
+
+# Environment Settings
+
+## Database configuration
+A MySQL DB has been added to the project, its credentials are:
+* user: root
+* password: toor
+* database name: db
+These credentials are used in **`docker-compose-yml`** and on the connection specified on the **`.env`** file.
+
+![db-docker-compose](https://i.imgur.com/puBGHdd.jpg)
+
+![config-db-symfony](https://i.imgur.com/FWDumvF.jpg)
+
+If using workbench it can be setup this way to work witht he DB:
+
+![workbench-config](https://i.imgur.com/kc16Ptb.png)
+
+## Start Stack (Symfony + MySQL + Nginx)
+On Linux or Mac, you can execute:
+```bash
+make start
+```
+On Windows, execute:
+```bash
+docker-compose up -d
+```
+
+## Stop Stack
+On Linux or Mac, you can execute:
+```bash
+make stop
+```
+On Windows, execute:
+```bash
+docker-compose down
+```
+
+## Interactive Mode
+On Linux or Mac, execute:
+```bash
+make interactive
+```
+On Windows, execute:
+```bash
+docker-compose -f docker-compose.cli.yml run \
+    --rm \
+    --no-deps \
+    -e HOST_USER=${UID} \
+    -e TERM=xterm-256color \
+    php-cli /bin/zsh -l
+```
+
+After starting the stack on interactive mode, execute **`composer install`**
+
+## Check the App is up and running
+To visualize the project go to:
+```
+http://localhost:8000/
+```
---
+++ src/Controller/IconicApiController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller;
+
+use phpDocumentor\Reflection\Types\True_;
+use RecursiveArrayIterator;
+use RecursiveIterator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class IconicApiController extends AbstractController
+{
+    private $httpClient;
+
+    public function __construct(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    // public function sort_items($a, $b)
+    // {
+    //     return ($a['video_count'], $b['video_count'])
+    // }
+
+    protected function getIconicProducts($gender, $page, $page_size)
+    {
+        $response = $this->httpClient->request(
+            'GET',
+            'https://eve.theiconic.com.au/catalog/products?gender='.$gender.'&page='.$page.'&page_size='.$page_size.'&sort=popularity'
+        );
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        $content = $response->toArray();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+        
+        $get_results = $this->_generateAbResults($content);
+
+        $video_count = array_column($get_results, 'video_count');
+        array_multisort($video_count, SORT_DESC, $get_results);
+
+        return json_encode($get_results);
+    }
+
+    protected function getIconicProductVideo($url)
+    {
+        $response = $this->httpClient->request(
+            'GET',
+            $url
+        );
+
+        $content = $response->toArray();
+
+        return $content;
+    }
+
+    private function _generateAbResults($args)
+    {
+        if (array_key_exists("_embedded", $args)) {
+            $count = 0;
+            $less = 0;
+            $res = [];
+            foreach ($args['_embedded'] as $item) {
+                foreach ($item as $prod) {
+                    $count++;
+                    $url = '';
+                    if ($prod['video_count'] > 0) {
+                        $url = $prod['_links']['self']['href']."/videos";        
+                        $get_video = $this->getIconicProductVideo($url);
+                        $video_url = $get_video['_embedded']['videos_url'][0]['url'];
+                        $prod["video_url"] = $video_url;
+                        array_push($res, $prod);
+                        array_pop($prod);
+                        $less++;
+                    } else {
+                        array_push($res, $prod);
+                    }
+                }
+            }
+
+            return $res;
+        }
+    }
+
+    /**
+     * @Route("/iconic/api", name="iconic_api_list")
+     */
+    public function list(Request $request)
+    {
+        $gender = $request->get('gender', 'female'); 
+        $page = $request->get('page', 1);
+        $page_size = $request->get('psize', 10);
+
+        $response = $this->getIconicProducts($gender, $page, $page_size);
+
+        return new JsonResponse(json_decode($response, true));
+    }
+}
